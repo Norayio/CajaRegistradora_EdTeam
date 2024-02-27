aaa70d74ab863afa15ce2c79e949edd0b16399ba
Norayio <pierresediaz@gmail.com>
2024-02-27 12:21:31 -0500

    clase abstracta con constructores, getters y setters, ya no se puede poner precios ni cantidades negativas

---
+++ CajaRegistradora/src/team/ed/objects/Product.java
@@ -0,0 +1,57 @@
+package team.ed.objects;
+
+public abstract class Product {
+
+    private String name; // nombre
+    private int amount; // cantidad
+    private double price; // precio
+
+
+    public Product(){ // constructor
+
+    }
+
+    public Product(String n){ // constructor
+        this.name = n;
+    }
+
+
+    //getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+
+        if (amount < 0){
+            System.out.println("No puedes asignar una cantidad negativa");
+            return; // en ez de el else, utilizamos el return.
+        }
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+
+        if (price < 0){
+            System.out.println("No puedes colocar una precio negativa");
+            return; // en ez de el else, utilizamos el return.
+        }
+        this.price = price;
+    }
+
+
+
+
+}
